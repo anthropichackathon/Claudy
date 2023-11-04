260f587cf1ef01a798077b1975ab641dc9b2ebee
bdoroszuk <bdoroszuk@inpost.pl>
2023-11-04 16:00:08 +0100

    claude utils

--- app/src/utils/claude.py
+++ app/src/utils/claude.py
@@ -1,10 +1,19 @@
 from anthropic import Anthropic
 import os
-from typing import Union, Optional
 
-def setup_anthropic() -> None:
+def get_anthropic():
     """Setup anthropic client"""
     api_key = os.getenv("CLAUDE_API_KEY")
     anthropic = Anthropic(api_key=api_key)
 
-def claude_call()
+    return anthropic
+
+
+def claude_call(llm_setup, prompt, model="claude-2", max_tokens = 300, **kwargs):
+    """Call Claude API"""
+    response = llm_setup.completions.create(
+        model=model,
+        max_tokens_to_sample=max_tokens,
+        prompt=prompt,
+    )
+    return response.completion

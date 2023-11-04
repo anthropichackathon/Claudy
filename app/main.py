ebc50a2d16cfca92d14810879052c7c736c3d426
Grzegorz Roguski <groguski@inpost.pl>
2023-11-04 16:36:30 +0000

    docker stuff, working empty containers

--- app/main.py
+++ app/main.py
@@ -1,90 +1,21 @@
-import tkinter as tk
+import logging
+import os
 
 from dotenv import load_dotenv
+from fastapi import FastAPI
+from weaviate import Client
 
-from app.src.KeyListener import KeyListener
-from src.buttons.Start import StartButton
-from src.buttons.Stop import StopButton
+from app.src.routers.main import get_basic_router
 
 load_dotenv()
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-def start_key_listener():
-    start_button.config(state=tk.DISABLED)
-    stop_button.config(state=tk.NORMAL)
-    key_listener.start()
+client = Client(os.getenv("WEAVIATE_DB_HOST"))
 
+app = FastAPI(title="Embedding query API")
+app.include_router(get_basic_router())
 
-def stop_key_listener():
-    stop_button.config(state=tk.DISABLED)
-    start_button.config(state=tk.NORMAL)
-    key_listener.stop()
-
-
-def on_close():
-    key_listener.stop()
-    root.destroy()
-
-
-# Create the main window
-root = tk.Tk()
-w = 480
-h = 320
-root.geometry(f"{w}x{h}")
-root.protocol("WM_DELETE_WINDOW", on_close)
-
-# Create the Text widget for logs
-log_text = tk.Text(root)
-log_text.grid(row=0, column=0, rowspan=6, sticky='nsew')
-
-# Create a key listener
-key_listener = KeyListener(log_text)
-
-# Create the start button first without griding it
-start_button = StartButton(root, key_listener=key_listener)
-
-# Then create the stop button and pass the start button to it
-stop_button = StopButton(root, key_listener=key_listener, start_button=start_button)
-stop_button.grid(row=0, column=2, sticky='new')
-
-# Now grid the start button and pass the stop button to it
-start_button.stop_button = stop_button
-start_button.grid(row=0, column=1, sticky='new')
-
-# Create more buttons
-button2 = tk.Button(root, text='Daily summary')
-button2.grid(row=1, column=1, sticky='nsew', columnspan=2)
-
-button3 = tk.Button(root, text='Consolidate knowledge')
-button3.grid(row=2, column=1, sticky='nsew', columnspan=2)
-
-button4 = tk.Button(root, text='Help me')
-button4.grid(row=3, column=1, sticky='nsew', columnspan=2)
-
-button5 = tk.Button(root, text='Voice')
-button5.grid(row=4, column=1, sticky='sew')
-
-button6 = tk.Button(root, text='Snip')
-button6.grid(row=4, column=2, sticky='sew')
-
-# Create input for text
-input_text = tk.Entry(root)
-input_text.grid(row=5, column=0, sticky='nsew')
-
-# Create button for inputting image
-input_button = tk.Button(root, text='Input Image')
-input_button.grid(row=5, column=1, columnspan=2, sticky='ew')
-
-root.grid_rowconfigure(0, weight=1)  # make the Text widget expandable
-root.grid_rowconfigure(1, weight=1)
-root.grid_rowconfigure(2, weight=1)
-root.grid_rowconfigure(3, weight=1)
-root.grid_rowconfigure(4, weight=1)
-root.grid_rowconfigure(5, weight=1)
-
-root.grid_columnconfigure(0, weight=1)
-root.grid_columnconfigure(1, weight=0)
-root.grid_columnconfigure(2, weight=0)
-
-# Run the GUI
-root.mainloop()
+logger.info(f"Weaviate Client: {client}")
+logger.info(f"App works: {app}")
